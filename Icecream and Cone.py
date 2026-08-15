cc66d07421758d4f115e6b756fadbd153c57b3e2
its_Vinit not_Vineet <vinitkavathekar375@gmail.com>
2026-08-15 22:40:22 +0530

    Create Icecream and Cone.py
    solution of icecream and cone problem

---
+++ Icecream and Cone.py
@@ -0,0 +1,12 @@
+# cook your dish here
+x,y=map(int, input().split())
+if x>y:
+    print(y)
+elif x<y:
+    print(x)
+else:
+    print(x)
+    
+#     or
+# x, y = map(int, input().split())
+# print(min(x, y))
